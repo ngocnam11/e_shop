9274fafb2f4cfdef8770948e99068968cc98d8d8
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2022-08-19 00:37:50 +0700

    add more routes

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,32 +1,42 @@
-import 'package:e_shop/screens/forgot_password_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/screens.dart';
 
 abstract class AppRouter {
+  static const String home = '/';
   static const String login = '/login';
   static const String forgotPassword = '/forgot_password';
   static const String signup = '/signup';
-  static const String home = '/';
   static const String search = '/search';
   static const String cart = '/cart';
   static const String wishlist = '/wishlist';
-  static const String notification = '/notification';
+  static const String chat = '/chat';
   static const String profile = '/profile';
+  static const String notification = '/notification';
 
   static Route onGenerateRoute(RouteSettings settings) {
     debugPrint('Route: ${settings.name}');
     switch (settings.name) {
+      case home:
+        return _materialPageRoute(settings, HomeScreen());
       case login:
         return _materialPageRoute(settings, LoginScreen());
       case forgotPassword:
         return _materialPageRoute(settings, ForgotPasswordScreen());
       case signup:
         return _materialPageRoute(settings, SignupScreen());
-      case home:
-        return _materialPageRoute(settings, HomeScreen());
+      case search:
+        return _materialPageRoute(settings, SearchScreen());
+      case cart:
+        return _materialPageRoute(settings, CartScreen());
+      case wishlist:
+        return _materialPageRoute(settings, WishlistScreen());
+      case chat:
+        return _materialPageRoute(settings, ChatScreen());
       case profile:
         return _materialPageRoute(settings, ProfileScreen());
+      case notification:
+        return _materialPageRoute(settings, NotificationScreen());
       default:
         return _materialPageRoute(
           settings,
