1656fb1ea3cdf8527b91f1d58dd307ffa649523e
Ngoc Nam <naml11762@gmail.com>
2022-08-20 15:50:39 +0700

    update UI

--- lib/widgets/product_carousel.dart
+++ lib/widgets/product_carousel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'product_card.dart';
+
 class ProductCarousel extends StatelessWidget {
   const ProductCarousel({Key? key}) : super(key: key);
 
@@ -12,60 +14,12 @@ class ProductCarousel extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: 5,
         itemBuilder: (context, index) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    height: 160,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      color: Colors.grey,
-                    ),
-                    child: Text(''),
-                  ),
-                  Positioned(
-                    top: 8,
-                    right: 8,
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.blue[300],
-                        shape: BoxShape.circle,
-                      ),
-                      child: FittedBox(
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.favorite_outline,
-                            color: Colors.red[400],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                'Product name',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              Text(
-                'Description',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              Text(
-                '\$Price',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ],
-          );
+          return ProductCard();
         },
         separatorBuilder: (context, index) => const SizedBox(width: 24),
       ),
     );
   }
 }
+
+
