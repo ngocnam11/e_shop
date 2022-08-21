74a1a065c7ed0871ef74c6d734851dbc4ecaf1c1
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2022-08-21 23:53:32 +0700

    new widget for empty cart and wishlist

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'empty_product.dart';
 import '../router/router.dart';
 import '../widgets/list_item.dart';
 
 class CartScreen extends StatelessWidget {
-  const CartScreen({Key? key}) : super(key: key);
+  CartScreen({Key? key}) : super(key: key);
 
   static MaterialPageRoute route() {
     return MaterialPageRoute(
       settings: const RouteSettings(name: AppRouter.cart),
-      builder: (_) => const CartScreen(),
+      builder: (_) => CartScreen(),
     );
   }
+
+  final Map<String, dynamic> product = {};
 
   @override
   Widget build(BuildContext context) {
@@ -25,73 +28,95 @@ class CartScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.separated(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        itemCount: 6,
-        itemBuilder: (context, index) {
-          return ListItem(
-            child: Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.remove_circle_outline),
-                ),
-                Text(
-                  '1',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add_circle_outline),
-                ),
-              ],
+      body: product.keys.isEmpty
+          ? const EmptyProduct()
+          : ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              itemCount: product.keys.length,
+              itemBuilder: (context, index) {
+                return ListItem(
+                  product: product,
+                  index: index,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.remove_circle_outline),
+                      ),
+                      Text(
+                        '1',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.add_circle_outline),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
             ),
-          );
-        },
-        separatorBuilder: (context, index) => const SizedBox(height: 12),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(
-          left: 24,
-          right: 24,
-          bottom: 28,
-          top: 12,
-        ),
-        child: SizedBox(
-          height: 80,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'Total',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  Text(
-                    '{price}\$',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () {
+      bottomNavigationBar: product.keys.isEmpty
+          ? Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                onLongPress: () {
                   Navigator.of(context).pushNamed(AppRouter.checkout);
                 },
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    AppRouter.home,
+                    (route) => false,
+                  );
+                },
                 style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 16,
-                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 16),
                   primary: Colors.blueAccent.shade100,
-                  fixedSize: const Size.fromWidth(500),
                 ),
                 child: const Text('Continue Shopping'),
               ),
-            ],
-          ),
-        ),
-      ),
+            )
+          : Padding(
+              padding: const EdgeInsets.only(
+                left: 24,
+                right: 24,
+                bottom: 28,
+                top: 12,
+              ),
+              child: SizedBox(
+                height: 80,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Total',
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                        Text(
+                          '{price}\$',
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(AppRouter.checkout);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        primary: Colors.blueAccent.shade100,
+                        fixedSize: const Size.fromWidth(500),
+                      ),
+                      child: const Text('Go to checkout'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
     );
   }
 }
--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'empty_product.dart';
 import '../router/router.dart';
 import '../widgets/custom_navigationbar.dart';
 import '../widgets/list_item.dart';
 
 class WishlistScreen extends StatelessWidget {
-  const WishlistScreen({Key? key}) : super(key: key);
+  WishlistScreen({Key? key}) : super(key: key);
 
   static MaterialPageRoute route() {
     return MaterialPageRoute(
       settings: const RouteSettings(name: AppRouter.wishlist),
-      builder: (_) => const WishlistScreen(),
+      builder: (_) => WishlistScreen(),
     );
   }
+
+  final Map<String, dynamic> product = {'Product name': 'Size: L'};
 
   @override
   Widget build(BuildContext context) {
@@ -26,19 +29,23 @@ class WishlistScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.separated(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        itemCount: 6,
-        itemBuilder: (context, index) {
-          return ListItem(
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('Add to Cart'),
+      body: product.keys.isEmpty
+          ? const EmptyProduct()
+          : ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              itemCount: product.keys.length,
+              itemBuilder: (context, index) {
+                return ListItem(
+                  product: product,
+                  index: index,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Add to Cart'),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
             ),
-          );
-        },
-        separatorBuilder: (context, index) => const SizedBox(height: 12),
-      ),
       bottomNavigationBar: const CustomNavigationBar(
         currentRoute: AppRouter.wishlist,
       ),
