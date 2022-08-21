45467d8154531402759652fab4dde706e2703f6f
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2022-08-21 10:02:46 +0700

    set default Android device orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
 
 import 'config/theme.dart';
 import 'router/router.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  }
   runApp(const MyApp());
 }
 
