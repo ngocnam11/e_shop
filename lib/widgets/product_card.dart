2e3de9b5843b0f206594f8c00073981e21030cdf
Ngoc Nam <naml11762@gmail.com>
2022-08-26 22:11:52 +0700

    update widgets

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../router/router.dart';
+
 class ProductCard extends StatelessWidget {
   const ProductCard({
     Key? key,
@@ -7,56 +9,61 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Stack(
-          children: [
-            Container(
-              height: 160,
-              width: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.grey,
-              ),
-              child: Text(''),
-            ),
-            Positioned(
-              top: 8,
-              right: 8,
-              child: Container(
-                height: 30,
-                width: 30,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pushNamed(AppRouter.product);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 160,
+                width: 150,
                 decoration: BoxDecoration(
-                  color: Colors.blue[300],
-                  shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.grey,
                 ),
-                child: FittedBox(
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.favorite_outline,
-                      color: Colors.red[400],
+                child: Text(''),
+              ),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: Container(
+                  height: 30,
+                  width: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[300],
+                    shape: BoxShape.circle,
+                  ),
+                  child: FittedBox(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite_outline,
+                        color: Colors.red[400],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        Text(
-          'Product name',
-          style: Theme.of(context).textTheme.headline4,
-        ),
-        Text(
-          'Description',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        Text(
-          '\$Price',
-          style: Theme.of(context).textTheme.headline5,
-        ),
-      ],
+            ],
+          ),
+          Text(
+            'Product name',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Text(
+            'Description',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          Text(
+            '\$Price',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        ],
+      ),
     );
   }
 }
