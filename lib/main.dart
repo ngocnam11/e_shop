f2424d7a29e4a296066782e3ec46a1bdf2c1a1d4
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2022-08-26 22:05:19 +0700

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,23 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'config/theme.dart';
+import 'firebase_options.dart';
 import 'router/router.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   if (Platform.isAndroid) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   }
+
   runApp(const MyApp());
 }
 
