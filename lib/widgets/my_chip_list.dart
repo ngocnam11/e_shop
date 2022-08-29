4492660ce32c15b5c30a3156f5177e476d546e25
Ngoc Nam <naml11762@gmail.com>
2022-08-29 21:53:57 +0700

    new widget

---
+++ lib/widgets/my_chip_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyChipList<T> extends StatelessWidget {
+  const MyChipList({
+    Key? key,
+    required this.values,
+    required this.chipBuilder,
+  }) :super (key: key);
+
+  final List<T> values;
+  final Chip Function(T) chipBuilder;
+
+  List<Widget> _buildChipList() {
+    final List<Widget> items = [];
+    for (T value in values) {
+      items.add(chipBuilder(value));
+    }
+    return items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: _buildChipList(),
+    );
+  }
+}
