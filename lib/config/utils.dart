aa0def6d62e6f413da2610821cf7cd80b8609fe8
Ngoc Nam <naml11762@gmail.com>
2022-08-29 21:44:28 +0700

    update function

--- lib/config/utils.dart
+++ lib/config/utils.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 pickImage(ImageSource source) async {
-  final ImagePicker _imagePicker = ImagePicker();
+  final ImagePicker imagePicker = ImagePicker();
 
-  XFile? _file = await _imagePicker.pickImage(source: source);
+  XFile? file = await imagePicker.pickImage(source: source);
 
-  if (_file != null) {
-    return _file.readAsBytes();
+  if (file != null) {
+    return file.readAsBytes();
   }
   print('No image selected');
 }
