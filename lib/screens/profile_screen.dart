6e12984d3c93bbb10c79d1d733a8ba1113d9b8b2
Ngoc Nam <naml11762@gmail.com>
2022-08-29 21:46:11 +0700

    update screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,16 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../router/router.dart';
+import '../services/auth_services.dart';
 import '../widgets/custom_navigationbar.dart';
 import '../widgets/profile_menu.dart';
+import 'adminPanel/ad_home_screen.dart';
+import 'screens.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  const ProfileScreen({Key? key, required this.uid}) : super(key: key);
+  final String uid;
 
   static MaterialPageRoute route() {
     return MaterialPageRoute(
       settings: const RouteSettings(name: AppRouter.profile),
-      builder: (_) => const ProfileScreen(),
+      builder: (_) => ProfileScreen(
+        uid: FirebaseAuth.instance.currentUser!.uid,
+      ),
     );
   }
 
@@ -27,157 +35,152 @@ class ProfileScreen extends StatelessWidget {
           ),
         ],
       ),
-
-      body:
-          // FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-          //   future: FirebaseFirestore.instance
-          //       .collection('users')
-          //       .doc(widget.uid)
-          //       .get(),
-          //   builder: (context, snapshot) {
-          //     if (snapshot.connectionState == ConnectionState.waiting) {
-          //       return const Center(
-          //         child: CircularProgressIndicator(),
-          //       );
-          //     }
-          //     if (snapshot.hasData) {
-          // return
-          Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width - 40,
-                  child: Card(
-                    elevation: 16,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.all(12),
-                          height: 88,
-                          width: 88,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: FirebaseFirestore.instance.collection('users').doc(uid).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasData) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: Card(
+                          elevation: 16,
+                          shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(16),
                           ),
-                          child: const Text(''),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(16),
+                                  child: Image.network(
+                                    snapshot.data!['photoUrl'],
+                                    height: 88,
+                                    width: 88,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    snapshot.data!['username'],
+                                    style:
+                                        Theme.of(context).textTheme.headline3,
+                                  ),
+                                  const SizedBox(height: 12),
+                                  Text(
+                                    snapshot.data!['email'],
+                                    style:
+                                        Theme.of(context).textTheme.headline6,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                        const SizedBox(width: 12),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                      ),
+                      const SizedBox(height: 40),
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 24),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          border:
+                              Border.all(color: Colors.grey[300]!, width: 2),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            Text(
-                              'Khanh Vu',
-                              style: Theme.of(context).textTheme.headline3,
+                            ProfileMenu(
+                              icon: Icons.person_outline_rounded,
+                              title: 'My Account',
+                              press: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => MyAccountScreen(
+                                      uid: FirebaseAuth.instance.currentUser!.uid,
+                                      isAdmin: snapshot.data!['isAdmin'],
+                                    ),
+                                  ),
+                                );
+                              },
                             ),
-                            const SizedBox(height: 12),
-                            Text(
-                              'email cua Khanh Vu',
-                              style: Theme.of(context).textTheme.headline6,
+                            const SizedBox(height: 10),
+                            snapshot.data!['isAdmin']
+                                ? ProfileMenu(
+                                    icon: Icons.admin_panel_settings_outlined,
+                                    title: 'Go to Admin Panel',
+                                    press: () {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: ((context) =>
+                                              const AdHomeScreen()),
+                                        ),
+                                      );
+                                    },
+                                  )
+                                : const SizedBox(),
+                            const SizedBox(height: 10),
+                            ProfileMenu(
+                              icon: Icons.logout,
+                              title: 'Log out',
+                              press: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: const Text('Log out'),
+                                    content:
+                                        const Text('Do you want to log out?'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () =>
+                                            Navigator.pop(context, 'Cancel'),
+                                        child: const Text(
+                                          'No',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          AuthServices().logout();
+                                          Navigator.of(context)
+                                              .pushNamed(AppRouter.login);
+                                        },
+                                        child: const Text(
+                                          'Yes',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 24),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: Colors.grey[300]!, width: 2),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ProfileMenu(
-                        icon: Icons.person_outline_rounded,
-                        title: 'My Account',
-                        press: () {
-                          // Navigator.of(context).push(
-                          //   MaterialPageRoute(
-                          //     builder: (context) => MyAccountScreen(
-                          //       uid: FirebaseAuth.instance.currentUser!.uid,
-                          //       isAdmin: snapshot.data!['isAdmin'],
-                          //     ),
-                          //   ),
-                          // );
-                        },
-                      ),
-                      const SizedBox(height: 10),
-                      // snapshot.data!['isAdmin']
-                      // ?
-                      ProfileMenu(
-                        icon: Icons.admin_panel_settings_outlined,
-                        title: 'Go to Admin Panel',
-                        press: () {
-                          // Navigator.of(context).push(
-                          //   MaterialPageRoute(
-                          //     builder: ((context) =>
-                          //         const AdHomeScreen()),
-                          //   ),
-                          // );
-                        },
-                      ),
-                      // : const SizedBox(),
-                      const SizedBox(height: 10),
-                      ProfileMenu(
-                        icon: Icons.logout,
-                        title: 'Log out',
-                        press: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: const Text('Log out'),
-                              content: const Text('Do you want to log out?'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () =>
-                                      Navigator.pop(context, 'Cancel'),
-                                  child: const Text(
-                                    'No',
-                                    style: TextStyle(color: Colors.black),
-                                  ),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    // AuthServices().logout();
-                                    // Navigator.of(context).push(
-                                    //   MaterialPageRoute(
-                                    //     builder: (context) =>
-                                    //         const LoginScreen(),
-                                    //   ),
-                                    // );
-                                  },
-                                  child: const Text(
-                                    'Yes',
-                                    style: TextStyle(color: Colors.black),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+            );
+          }
+          return const SizedBox();
+        },
       ),
-      // }
-      // return const SizedBox();
-      // },
-      // ),
       bottomNavigationBar: const CustomNavigationBar(
         currentRoute: AppRouter.profile,
       ),
