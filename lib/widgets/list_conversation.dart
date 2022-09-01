740e6f7dd3d6a88f9f3304e41c0ce5f0dd6cd597
Ngoc Nam <naml11762@gmail.com>
2022-09-01 16:53:33 +0700

    update widget

--- lib/widgets/list_conversation.dart
+++ lib/widgets/list_conversation.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/screens.dart';
+import '../services/auth_services.dart';
 
 class ListConversation extends StatelessWidget {
   const ListConversation({
@@ -9,55 +11,83 @@ class ListConversation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.only(bottom: 12),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ConversationScreen(),),);
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 32,
-                  backgroundColor: Colors.amber,
-                  child: Text(index.toString()),
-                ),
-                SizedBox(
-                  height: 60,
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Username',
-                        style: Theme.of(context).textTheme.headline4,
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .where('uid', isNotEqualTo: AuthServices().user.uid)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+        return snapshot.data!.docs.toList().isEmpty
+            ? const Center(
+                child: Text('No discussion'),
+              )
+            : ListView.separated(
+                padding: const EdgeInsets.only(bottom: 12),
+                itemCount: snapshot.data!.docs.toList().length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => ConversationScreen(
+                            user: snapshot.data!.docs.toList()[index],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 32,
+                            backgroundColor: Colors.amber,
+                            backgroundImage: NetworkImage(
+                              snapshot.data!.docs.toList()[index]['photoUrl'],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 60,
+                            width: MediaQuery.of(context).size.width / 2,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  snapshot.data!.docs.toList()[index]['username'],
+                                  style: Theme.of(context).textTheme.headline4,
+                                ),
+                                Text(
+                                  'Message',
+                                  style: Theme.of(context).textTheme.headline6,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                          Text(
+                            '11/11/2022',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                        ],
                       ),
-                      Text(
-                        'Message',
-                        style: Theme.of(context).textTheme.headline6,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Text(
-                  '11/11/2022',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ],
-            ),
-          ),
-        );
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => const Divider(indent: 80),
+              );
       },
-      separatorBuilder: (context, index) => const Divider(indent: 80),
     );
   }
 }
