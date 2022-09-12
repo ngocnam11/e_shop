76e809c4dbc26dc0ffd60ff7e7d7bc8766f6b6e5
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2022-09-12 23:47:32 +0700

    add search to appbar

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -5,21 +5,21 @@ import '../router/router.dart';
 import '../services/firestore_services.dart';
 import '../widgets/carousel.dart';
 import '../widgets/list_categoties.dart';
-import '../widgets/text_field_input.dart';
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen({Key? key}) : super(key: key);
+  SearchScreen({Key? key}) : super(key: key);
 
   static MaterialPageRoute route() {
     return MaterialPageRoute(
       settings: const RouteSettings(name: AppRouter.search),
-      builder: (_) => const SearchScreen(),
+      builder: (_) => SearchScreen(),
     );
   }
 
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    final TextEditingController searchController = TextEditingController();
     List<String> category = [
       'All',
       'Men',
@@ -30,20 +30,40 @@ class SearchScreen extends StatelessWidget {
       'Laptop',
       'Book'
     ];
+
     return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 40,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(
+                Icons.search,
+                color: Colors.black54,
+              ),
+              suffixIcon: IconButton(
+                icon: const Icon(
+                  Icons.clear,
+                  color: Colors.black54,
+                ),
+                onPressed: () {
+                  _searchController.clear();
+                },
+              ),
+              hintText: 'Search...',
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(24, 32, 24, 0),
-              child: TextFieldInput(
-                controller: searchController,
-                hintText: 'Search Products',
-                textInputType: TextInputType.text,
-                prefixIcon: const Icon(Icons.search),
-              ),
-            ),
-            const SizedBox(height: 16),
             SizedBox(
               height: 32,
               child: ListView.separated(
