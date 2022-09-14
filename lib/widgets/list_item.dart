191e3cf126e900dec8625a1021e24b4f16543ec4
Ngoc Nam <naml11762@gmail.com>
2022-09-14 16:55:06 +0700

    update widgets

--- lib/widgets/list_item.dart
+++ lib/widgets/list_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../models/cart.dart';
 import '../models/product.dart';
+import '../services/firestore_services.dart';
 
 class ListItem extends StatelessWidget {
   const ListItem({
@@ -10,7 +12,7 @@ class ListItem extends StatelessWidget {
   }) : super(key: key);
 
   final Widget child;
-  final Product product;
+  final CartItem product;
 
   @override
   Widget build(BuildContext context) {
@@ -23,66 +25,77 @@ class ListItem extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image(
-                height: 100,
-                width: 100,
-                image: NetworkImage(
-                  product.imageUrl,
+        child: FutureBuilder<Product>(
+          future: FireStoreServices().getProductById(id: product.productId),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+            return Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image(
+                    height: 100,
+                    width: 100,
+                    image: NetworkImage(
+                      snapshot.data!.imageUrl,
+                    ),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                fit: BoxFit.cover,
-              ),
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: Text(
-                          product.name,
-                          style: Theme.of(context).textTheme.headline4,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              snapshot.data!.name,
+                              style: Theme.of(context).textTheme.headline4,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          // Checkbox(
+                          //   value: false,
+                          //   onChanged: (value) {},
+                          // ),
+                          const SizedBox(
+                            height: 32,
+                            width: 32,
+                          ),
+                        ],
                       ),
-                      // Checkbox(
-                      //   value: false,
-                      //   onChanged: (value) {},
-                      // ),
-                      const SizedBox(
-                        height: 32,
-                        width: 32,
-                      ),
-                    ],
-                  ),
-                  product.colors!.isEmpty && product.size!.isEmpty
-                      ? const SizedBox()
-                      : Text(
-                          '${product.colors![0]}, ${product.size![0]}',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
                       Text(
-                        '\$${product.price}',
-                        style: Theme.of(context).textTheme.headline5,
+                        '${product.color![0]}, ${product.size![0]}',
+                        style: Theme.of(context).textTheme.bodyText1,
                       ),
-                      child,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '\$${product.price}',
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                          child,
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
-            ),
-          ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
