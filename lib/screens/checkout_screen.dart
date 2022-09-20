51d4d8448ed1eb52d0e0efdf3dae1bb60bf2145c
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2022-09-20 10:46:42 +0700

    remove unused import

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -11,7 +11,6 @@ import '../services/auth_services.dart';
 import '../services/firestore_services.dart';
 import '../widgets/list_item.dart';
 import '../widgets/order_summary.dart';
-import 'screens.dart';
 
 class CheckoutScreen extends StatefulWidget {
   const CheckoutScreen({Key? key, this.deliveryAddress}) : super(key: key);
@@ -71,6 +70,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
       Navigator.of(context).pushNamed(AppRouter.orderConfirm);
     }
   }
+
   String deliveryAddress = 'Choose delivery address';
 
   @override
