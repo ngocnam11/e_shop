afd376b2045990fd76c6694781bcb564eba4808c
Ngoc Nam <naml11762@gmail.com>
2023-06-17 09:30:38 +0700

    add shared preferences

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:uuid/uuid.dart';
 
-import '../data/models/cart.dart';
 import '../data/models/delivery_address.dart';
 import '../data/models/message.dart';
 import '../data/models/order.dart';
@@ -22,12 +21,6 @@ class FireStoreServices {
     final snap = await _firestore.collection('users').doc(uid).get();
     UserModel user = UserModel.fromJson(snap.data()!);
     return user;
-  }
-
-  Future<Cart> getCartByUid({required String uid}) async {
-    final snap = await _firestore.collection('carts').doc(uid).get();
-    Cart cart = Cart.fromJson(snap.data()!);
-    return cart;
   }
 
   Future<Product> getProductById({required String id}) async {
@@ -59,29 +52,28 @@ class FireStoreServices {
     return recommendedProducts;
   }
 
-  Stream<List<Order>> getCurrentUserOrders() {
+  Stream<List<OrderModel>> getCurrentUserOrders() {
     final snaps = _firestore
         .collection('orders')
         .where('customerId', isEqualTo: _auth.currentUser!.uid)
         .snapshots();
-    final orders = snaps.map(
-        (snap) => snap.docs.map((doc) => Order.fromJson(doc.data())).toList());
+    final orders = snaps.map((snap) =>
+        snap.docs.map((doc) => OrderModel.fromJson(doc.data())).toList());
     return orders;
   }
 
-  Stream<List<Order>> getOrdersOfSellerId() {
+  Stream<List<OrderModel>> getOrdersOfSellerId() {
     final snaps = _firestore
         .collection('orders')
         .where('sellerId', isEqualTo: _auth.currentUser!.uid)
         .snapshots();
-    final orders = snaps.map(
-        (snap) => snap.docs.map((doc) => Order.fromJson(doc.data())).toList());
+    final orders = snaps.map((snap) =>
+        snap.docs.map((doc) => OrderModel.fromJson(doc.data())).toList());
     return orders;
   }
 
   Future<String> addProduct({
     required String uid,
-    // required int id,
     required String name,
     required String category,
     required double price,
@@ -116,222 +108,6 @@ class FireStoreServices {
     }
 
     return res;
-  }
-
-  Future<String> addProductToCart({
-    required String productId,
-    required String color,
-    required String size,
-    required int quantity,
-    required double price,
-    required String sellerId,
-  }) async {
-    String res = 'Some error occurred';
-    try {
-      final uid = _auth.currentUser!.uid;
-      CartItem newProduct = CartItem(
-        id: 'ci$productId',
-        productId: productId,
-        color: color,
-        size: size,
-        quantity: quantity,
-        price: price,
-        sellerId: sellerId,
-      );
-      Cart cart = Cart(
-        uid: uid,
-        products: [newProduct],
-      );
-      final snap = await _firestore.collection('carts').doc(uid).get();
-      if (snap.data() == null) {
-        _firestore
-            .collection('carts')
-            .doc(uid)
-            .set(Cart(uid: uid, products: const []).toJson());
-      }
-
-      final cartData = Cart.fromJson(snap.data()!);
-      if (cartData.products.isNotEmpty) {
-        print(cartData.products);
-        final products = cartData.products;
-        for (var i = 0; i < products.length; i++) {
-          if (products[i].id == 'ci$productId' &&
-              products[i].color == color &&
-              products[i].size == size) {
-            var quantity = products[i].quantity + newProduct.quantity;
-            var price = products[i].price + newProduct.price + .0;
-
-            final tmp = CartItem(
-              id: 'ci$productId',
-              productId: productId,
-              color: color,
-              size: size,
-              quantity: quantity,
-              price: price,
-              sellerId: sellerId,
-            );
-            products.removeAt(i);
-            print(products);
-            products.insert(i, tmp);
-            print(products);
-
-            _firestore.collection('carts').doc(uid).set(
-                  Cart(uid: uid, products: products).toJson(),
-                  SetOptions(merge: true),
-                );
-          } else if (products[i].id == 'ci${productId}p${color}c${size}s' &&
-              products[i].color == color &&
-              products[i].size == size) {
-            var quantity = products[i].quantity + newProduct.quantity;
-            var price = products[i].price + newProduct.price + .0;
-
-            final tmp = CartItem(
-              id: 'ci${productId}p${color}c${size}s',
-              productId: productId,
-              color: color,
-              size: size,
-              quantity: quantity,
-              price: price,
-              sellerId: sellerId,
-            );
-
-            products.removeAt(i);
-            products.insert(i, tmp);
-
-            _firestore.collection('carts').doc(uid).set(
-                  Cart(uid: uid, products: products).toJson(),
-                  SetOptions(merge: true),
-                );
-          } else if ((products[i].id == 'ci$productId' &&
-                  products[i].color != color &&
-                  products[i].size == size) ||
-              (products[i].id == 'ci$productId' &&
-                  products[i].size != size &&
-                  products[i].color == color)) {
-            CartItem product = CartItem(
-              id: 'ci${productId}p${color}c${size}s',
-              productId: productId,
-              color: color,
-              size: size,
-              quantity: quantity,
-              price: price + .0,
-              sellerId: sellerId,
-            );
-            _firestore.collection('carts').doc(uid).update({
-              'products': FieldValue.arrayUnion([product.toJson()]),
-            });
-          } else {
-            CartItem product = CartItem(
-              id: 'ci$productId',
-              productId: productId,
-              color: color,
-              size: size,
-              quantity: quantity,
-              price: price + .0,
-              sellerId: sellerId,
-            );
-            _firestore.collection('carts').doc(uid).update({
-              'products': FieldValue.arrayUnion([product.toJson()]),
-            });
-          }
-        }
-      } else {
-        _firestore.collection('carts').doc(uid).set(cart.toJson());
-      }
-
-      res = 'success';
-    } catch (e) {
-      res = e.toString();
-    }
-
-    return res;
-  }
-
-  Future<String> removeProductFromCart({
-    required String id,
-    required String productId,
-    required String color,
-    required String size,
-    required double priceOfItem,
-    required String sellerId,
-  }) async {
-    String resDel = 'Some error occurred';
-    try {
-      final snap = await _firestore
-          .collection('carts')
-          .doc(_auth.currentUser!.uid)
-          .get();
-      final cartData = Cart.fromJson(snap.data()!);
-      final products = cartData.products;
-      for (var i = 0; i < products.length; i++) {
-        if (products[i].id == id &&
-            products[i].color == color &&
-            products[i].size == size) {
-          var quantity = products[i].quantity - 1;
-          var price = products[i].price - priceOfItem + .0;
-
-          final tmp = CartItem(
-            id: id,
-            productId: productId,
-            color: color,
-            size: size,
-            quantity: quantity,
-            price: price,
-            sellerId: sellerId,
-          );
-          products.removeAt(i);
-          print(products);
-          products.insert(i, tmp);
-          print(products);
-
-          _firestore.collection('carts').doc(_auth.currentUser!.uid).set(
-                Cart(uid: _auth.currentUser!.uid, products: products).toJson(),
-                SetOptions(merge: true),
-              );
-        }
-      }
-      resDel = 'success';
-    } catch (e) {
-      resDel = e.toString();
-    }
-    return resDel;
-  }
-
-  Future<String> addOrder({
-    required String customerId,
-    required String sellerId,
-    required List<Product> products,
-    // required String paymentMethod,
-    required String deliveryAddress,
-    required double subtotal,
-    required double deliveryFee,
-    required double total,
-    required String orderStatus,
-  }) async {
-    String response = 'Some error occurred';
-    try {
-      var date = DateTime.now().toLocal();
-      final id = 'ORD${date.year}Y${date.month}M${date.day}ES${date.second}';
-      Order order = Order(
-        id: id,
-        customerId: customerId,
-        sellerId: sellerId,
-        products: products,
-        paymentMethod: 'Cash Money',
-        deliveryAddress: deliveryAddress,
-        subtotal: subtotal,
-        deliveryFee: deliveryFee,
-        total: total,
-        orderStatus: orderStatus,
-        createdAt: Timestamp.now(),
-      );
-      _firestore.collection('orders').doc(id).set(order.toJson());
-      response = 'success';
-    } catch (e) {
-      response = e.toString();
-    }
-
-    return response;
   }
 
   Future<String> addDeliveryAddress({
